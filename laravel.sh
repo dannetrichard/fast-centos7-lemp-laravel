ddbb2ac1ff05e063feba38739e18d6a4e2c11f07
dannetrichard <jinjun0927@126.com>
2017-11-30 18:23:27 +0800

    upload

---
+++ laravel.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+function laravel(){
+    mkdir -p /data/www/$1.jingyi-good.com 
+    cd /data/www/$1.jingyi-good.com
+    rm -rf $2
+    composer create-project --prefer-dist laravel/$2
+    cd $2
+    if [ "$2" == "lumen" ];then
+    lumen_cfg $1
+    else
+    laravel_cfg $1
+    fi
+    composer dump-autoload
+    cd 
+    nginx_cfg $1 $2
+}
+function lumen_cfg(){
+    chmod -R 777 storage
+    sed -i 's/UTC/Asia\/Shanghai\nAPP_ID=wxa02ce99b50401101\nAPP_SECRET=5c9e00d42a74132b5f153c49c8f32be6/g' .env
+    sed -i 's/DB_DATABASE=homestead/DB_DATABASE='$1'/g' .env
+    sed -i 's/DB_USERNAME=homestead/DB_USERNAME=root/g' .env
+    sed -i 's/DB_PASSWORD=secret/DB_PASSWORD=jinjun123/g' .env 
+}
+function laravel_cfg(){
+    chmod -R 777 storage
+    chmod -R 777 bootstrap/cache
+    sed -i 's/utf8mb4/utf8/g' config/database.php
+    sed -i 's/DB_DATABASE=homestead/DB_DATABASE='$1'/g' .env
+    sed -i 's/DB_USERNAME=homestead/DB_USERNAME=root/g' .env
+    sed -i 's/DB_PASSWORD=secret/DB_PASSWORD=jinjun123/g' .env        
+    sed -i 's/PUSHER_APP_SECRET=/PUSHER_APP_SECRET=\nAPP_ID=wxa02ce99b50401101\nAPP_SECRET=5c9e00d42a74132b5f153c49c8f32be6/g' .env
+    sed -i 's/UTC/Asia\/Shanghai/g' config/app.php
+}
+function nginx_cfg(){
+    cat >/etc/nginx/conf.d/$1.jingyi-good.com.conf <<EOF
+server {
+    listen   80;
+    server_name  $1.jingyi-good.com;
+
+    # note that these lines are originally from the "location /" block
+    root   /data/www/$1.jingyi-good.com/$2/public;
+    index index.php index.html index.htm;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+    error_page 404 /404.html;
+    error_page 500 502 503 504 /50x.html;
+    location = /50x.html {
+        root /usr/share/nginx/html;
+    }
+
+    location ~ \.php$ {
+        try_files \$uri =404;
+        fastcgi_pass unix:/var/run/php-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}
+EOF
+}
+laravel api lumen
+laravel tiger laravel
+laravel bee laravel
---
+++ lemp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#centos 7
+#https://www.google.co.jp/amp/s/www.hostinger.com/tutorials/how-to-install-lemp-centos7/amp/
+#nginx
+yum install epel-release -y
+yum install nginx -y
+systemctl start nginx
+systemctl enable nginx
+#certbot
+#pip install requests urllib3 pyOpenSSL --force --upgrade
+#yum -y install yum-utils
+#yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
+#sudo yum install certbot-nginx -y
+
+#mysql(mariadb)
+yum install mariadb-server mariadb -y
+systemctl start mariadb
+systemctl enable mariadb
+mysql_secure_installation
+#password jinjun123
+
+#php
+wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+rpm -Uvh remi-release-7.rpm
+yum install yum-utils -y
+yum-config-manager --enable remi-php71
+yum --enablerepo=remi,remi-php71 install php-fpm php-common
+yum --enablerepo=remi,remi-php71 install php-opcache php-pecl-apcu php-cli php-pear php-pdo php-mysqlnd php-pgsql php-pecl-mongodb php-pecl-redis php-pecl-memcache php-pecl-memcached php-gd php-mbstring php-mcrypt php-xml php-Tokenizer -y
+
+sed -i 's/user = apache/user = nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/group = apache/group = nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/listen.owner = nobody/listen.owner = nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/listen.group = nobody/listen.group = nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9000\nlisten = \/var\/run\/php-fpm.sock/g' /etc/php-fpm.d/www.conf
+systemctl start php-fpm.service
+systemctl enable php-fpm.service
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+composer config -g repo.packagist composer https://packagist.phpcomposer.com
